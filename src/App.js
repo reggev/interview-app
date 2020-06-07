88e605001a8647b6b72ac14a4f9783bb85f3eeda
reggev <reggev.am@gmail.com>
2020-06-07 11:41:56 +0300

    added all the files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>interview</h1>
+        <h1>[Taxes Ranges]</h1>
         <p>
           Please follow the design using
           <a
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+import './index.scss';
+import './fonts.scss';
 import App from './App';
 import store from './store';
 
